fbfd5fc4620d0f93519e0ce1c7a809ab46c7e6cc
Ahmad EL Masri <ahmadjelmasri@gmail.com>
2016-12-13 08:50:47 +0200

    style: separate test and class

--- src/vector.py
+++ src/vector.py
@@ -48,32 +48,3 @@ class Vector(object):
         except ZeroDivisionError:
             raise Exception('Cannot normalize the zero vector')
 
-
-# Tester
-# Addition
-v = Vector([8.218, -9.341])
-w = Vector([-1.129, 2.111])
-print v.plus(w)
-
-# Subtraction 
-v = Vector([7.119, 8.125])
-w = Vector([-8.223, 0.878])
-print v.minus(w)
-
-# Scaling 
-v = Vector([1.671, -1.012, -0.318])
-c = 7.41
-print v.times_scalar(c)
-
-v = Vector([-0.221, 7.437])
-print v.get_magnitude()
-
-v = Vector([8.813, -1.331, -6.247])
-print v.get_magnitude()
-
-v = Vector([5.581, -2.136])
-print v.normalize_vector()
-
-
-v = Vector([1.996, 3.108, -4.554])
-print v.normalize_vector()
