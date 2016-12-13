f58c491ad716f603c96abd9ca0165d57ee86d568
Ahmad EL Masri <ahmadjelmasri@gmail.com>
2016-12-13 10:39:59 +0200

    feat: add angle and dot product calculation

--- src/test.py
+++ src/test.py
@@ -2,30 +2,53 @@ from vector import Vector
 
 
 # Tester
+
 # Addition
 v = Vector([8.218, -9.341])
 w = Vector([-1.129, 2.111])
-print v.plus(w)
+#print v.plus(w)
 
 # Subtraction
 v = Vector([7.119, 8.125])
 w = Vector([-8.223, 0.878])
-print v.minus(w)
+#print v.minus(w)
 
 # Scaling
 v = Vector([1.671, -1.012, -0.318])
 c = 7.41
-print v.times_scalar(c)
+#print v.times_scalar(c)
 
+# Magnitude
 v = Vector([-0.221, 7.437])
-print v.get_magnitude()
+#print v.get_magnitude()
 
 v = Vector([8.813, -1.331, -6.247])
-print v.get_magnitude()
+#print v.get_magnitude()
+#print v.get_magnitude_using_dot_product()
 
+# Normalization
 v = Vector([5.581, -2.136])
-print v.normalize_vector()
+#print v.normalize_vector()
 
 
 v = Vector([1.996, 3.108, -4.554])
-print v.normalize_vector()
+#print v.normalize_vector()
+
+
+# dot Product
+v = Vector([7.887, 4.138])
+w = Vector([-8.802, 6.776])
+print v.dot_product(w)
+
+v = Vector([-5.955, -4.904, -1.874])
+w = Vector([-4.496, -8.755, 7.103])
+print v.dot_product(w)
+
+# Angle
+v = Vector([3.183, -7.627])
+w = Vector([-2.668, 5.319])
+print v.calculate_angle(w)
+
+v = Vector([7.35, 0.221, 5.188])
+w = Vector([2.751, 8.259, 3.985])
+print v.calculate_angle(w, True)
--- src/vector.py
+++ src/vector.py
@@ -1,4 +1,4 @@
-from math import sqrt
+from math import sqrt, acos, degrees
 
 class Vector(object):
     def __init__(self, coordinates):
@@ -40,10 +40,31 @@ class Vector(object):
         coordinates_squared = [x**2 for x in self.coordinates]
         return sqrt(sum(coordinates_squared))
 
+    def get_magnitude_using_dot_product(self):
+        return sqrt(self.dot_product(self))
+
     def normalize_vector(self):
         try:
             magnitude = self.get_magnitude()
             return self.times_scalar(1./magnitude)
+
+        except ZeroDivisionError:
+            raise Exception('Cannot normalize the zero vector')
+
+    def dot_product(self, v):
+        dot_product_coordinates = [x*y for x,y in zip(self.coordinates,
+            v.coordinates)]
+        return sum(dot_product_coordinates)
+
+    def calculate_angle(self, v, degree = False):
+        try:
+            w_magnitude = self.get_magnitude()
+            v_magnitude = v.get_magnitude()
+            theta = acos(self.dot_product(v)/(w_magnitude*v_magnitude))
+            if degree:
+                return degrees(theta)
+            else:
+                return theta
 
         except ZeroDivisionError:
             raise Exception('Cannot normalize the zero vector')
